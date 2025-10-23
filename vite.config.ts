c70a24a4dbf6b993eb42b8b589ccfd4ba4298b65
trafficpre <65816926+trafficpre@users.noreply.github.com>
2025-10-23 17:57:21 +0800

    Updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   server: {
@@ -19,7 +18,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   resolve: {
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
   },
